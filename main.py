ca59aa4fd3049194027a0203a25e84dd8b1f50aa
tz1006 <a@omg.tf>
2018-01-14 23:02:04 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*- 
+# filename: main.py
+
+import checktime
+
+from sharelist_t import share_list
+import sort_price
+import sort_kdj
+
+share_list = sort_price.sort_list(share_list)
+share_list = sort_kdj.sort_list(share_list)
+
+last_ma = sort.last_ma
+
+from monitor import *
+ma_monitor_start(share_list, last_ma)
+
+
+import code
+code.interact(banner = "", local = locals())
